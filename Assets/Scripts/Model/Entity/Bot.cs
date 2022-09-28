4eced7ce582cabefcfc13d49046d53d1c038d7aa
CaptainKryga <krygadenis@gmail.com>
2022-09-28 22:55:28 +0200

    update entity parent's hierarchy

--- Assets/Scripts/Model/Entity/Bot.cs
+++ Assets/Scripts/Model/Entity/Bot.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Bot : MonoBehaviour
+namespace Model.Entity
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public class Bot : global::Model.Entity.Entity
+	{
+	
+	}
 }
--- Assets/Scripts/Model/Entity/Entity.cs
+++ Assets/Scripts/Model/Entity/Entity.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour
+namespace Model.Entity
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public class Entity : MonoBehaviour
+	{
+	
+	}
 }
