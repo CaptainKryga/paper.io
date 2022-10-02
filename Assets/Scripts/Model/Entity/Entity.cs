bf03fd527f1ca635ed998053fbc2dc82c8d8e141
CaptainKryga <krygadenis@gmail.com>
2022-10-02 12:05:59 +0200

    update move from one box

--- Assets/Scripts/Model/Entity/Entity.cs
+++ Assets/Scripts/Model/Entity/Entity.cs
@@ -17,15 +17,18 @@ namespace Model.Entity
 		{
 			transform.position = Vector3.MoveTowards(transform.position, movePoint.position,
 				moveSpeed * Time.deltaTime);
-			
-			if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+
+			if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
 			{
-				movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-			}
+				if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+				{
+					movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+				}
 			
-			if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
-			{
-				movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+				if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+				{
+					movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+				}
 			}
 		}
 	}
