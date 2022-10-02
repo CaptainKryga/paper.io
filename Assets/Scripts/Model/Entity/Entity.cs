f0b77f047bca07dd79ad5cba6c09b02dd73e7ecf
CaptainKryga <krygadenis@gmail.com>
2022-10-02 12:04:26 +0200

    add entity controller

--- Assets/Scripts/Model/Entity/Entity.cs
+++ Assets/Scripts/Model/Entity/Entity.cs
@@ -1,9 +1,32 @@
+using System;
 using UnityEngine;
 
 namespace Model.Entity
 {
 	public class Entity : MonoBehaviour
 	{
-	
+		public float moveSpeed = 5;
+		public Transform movePoint;
+
+		private void Start()
+		{
+			movePoint.parent = null;
+		}
+
+		private void Update()
+		{
+			transform.position = Vector3.MoveTowards(transform.position, movePoint.position,
+				moveSpeed * Time.deltaTime);
+			
+			if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+			{
+				movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+			}
+			
+			if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+			{
+				movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+			}
+		}
 	}
 }
