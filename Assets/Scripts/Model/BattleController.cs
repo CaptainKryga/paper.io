1b73d2bb2342675fe69aa709557800bab702ea73
CaptainKryga <krygadenis@gmail.com>
2022-10-06 18:54:09 +0200

    add random start pos

--- Assets/Scripts/Model/BattleController.cs
+++ Assets/Scripts/Model/BattleController.cs
@@ -1,9 +1,12 @@
 using System;
 using Model.Photon;
+using Model.TileMap;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
 namespace Model
 {
@@ -11,6 +14,7 @@ namespace Model
 	public class BattleController : MonoBehaviour
 	{
 		[SerializeField] private ReadyController readyController;
+		[SerializeField] private TileDataBase tileDataBase;
 
 		[SerializeField] private CustomRaiseEvents customRaiseEvents;
 
@@ -34,6 +38,23 @@ namespace Model
 			customRaiseEvents.ReadyUpdateAllPlayers_Action -= ReadyUpdateAllPlayers;
 			
 			customRaiseEvents.EndBattle_Action -= GameOver;
+		}
+
+		private Vector3Int[] startPoints;
+		private void Start()
+		{
+			startPoints = new Vector3Int[16];
+			int point = tileDataBase.sizeMap / 5;
+			for (int x = 0, i = 0; x < tileDataBase.sizeMap; x++)
+			{
+				for (int y = 0; y < tileDataBase.sizeMap; y++)
+				{
+					if (x != 0 && y != 0 && x % point == 0 && y % point == 0)
+					{
+						startPoints[i++] = new Vector3Int(x, y);
+					}
+				}
+			}
 		}
 
 		public void PlayerUpdateReady(int playerActor, bool isReady)
@@ -89,15 +110,27 @@ namespace Model
 		public void StartBattle()
 		{
 			Player[] players = PhotonNetwork.PlayerList;
-			foreach (var player in players)
+			RandomStartPositions();
+			for (int x = 0; x < players.Length; x++)
 			{
-				if ((bool) player.CustomProperties["isReady"])
+				if ((bool)players[x].CustomProperties["isReady"])
 				{
-					customRaiseEvents.Request_StartBattle(player.ActorNumber, new Vector3(5, 5, 0));
+					customRaiseEvents.Request_StartBattle(players[x].ActorNumber, startPoints[x]);
 				}
 			}
 
 			isBattle = true;
+		}
+
+		private void RandomStartPositions()
+		{
+			for (int x = 0; x < startPoints.Length; x++)
+			{
+				int rnd = Random.Range(0, startPoints.Length);
+				Vector3Int temp = startPoints[rnd];
+				startPoints[rnd] = startPoints[x];
+				startPoints[x] = temp;
+			}
 		}
 
 		public void GameOver()
